455e1b1839371f7105eea8a114c98d79aadac591
Yuta Adachi <utauta_628@yahoo.co.jp>
2016-12-06 11:48:45 +0900

    退室処理に対応しました

--- app/src/main/java/com/example/k13006kk/DBmonitoringsample/MainActivity.java
+++ app/src/main/java/com/example/k13006kk/DBmonitoringsample/MainActivity.java
@@ -14,9 +14,10 @@ import com.example.k13006kk.mylibrary.UserColumns;
 public class MainActivity extends Activity {
 
     DBaccess dBaccess = new DBaccess();
-    public String[] room = new String[6];
+    public String[] enterroom = new String[6];
+    public String[] exitroom = new String[6];
 
-    TextView tv1, tv2, tv3, tv4;
+    TextView tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,18 @@ public class MainActivity extends Activity {
 
         tv4 = (TextView) findViewById(R.id.nichiji);
         tv4.setText("null");
+
+        tv5 = (TextView) findViewById(R.id.toumei2);
+        tv5.setText("null");
+
+        tv6 = (TextView) findViewById(R.id.heyamei2);
+        tv6.setText("null");
+
+        tv7 = (TextView) findViewById(R.id.heyaban2);
+        tv7.setText("null");
+
+        tv8 = (TextView) findViewById(R.id.nichiji2);
+        tv8.setText("null");
     }
 
     ContentObserver mContentObserver = new ContentObserver(new Handler()) {
@@ -42,21 +55,36 @@ public class MainActivity extends Activity {
             super.onChange(selfChange);
             // 変更された時の処理を書く
             final ContentResolver resolver = getContentResolver();
-            for (int i = 0; i < dBaccess.monitoring(resolver).length; i++) {
-                room[i] = dBaccess.monitoring(resolver)[i];
+            for (int i = 0; i < dBaccess.getenterroom(resolver).length; i++) {
+                enterroom[i] = dBaccess.getenterroom(resolver)[i];
+            }
+            for (int i = 0; i < dBaccess.getexitroom(resolver).length; i++) {
+                exitroom[i] = dBaccess.getexitroom(resolver)[i];
             }
 
             tv1 = (TextView) findViewById(R.id.toumei);
-            tv1.setText(room[1]);
+            tv1.setText(enterroom[1]);
 
             tv2 = (TextView) findViewById(R.id.heyamei);
-            tv2.setText(room[2]);
+            tv2.setText(enterroom[2]);
 
             tv3 = (TextView) findViewById(R.id.heyaban);
-            tv3.setText(room[3]);
+            tv3.setText(enterroom[3]);
 
             tv4 = (TextView) findViewById(R.id.nichiji);
-            tv4.setText(room[4]);
+            tv4.setText(enterroom[4]);
+
+            tv5 = (TextView) findViewById(R.id.toumei2);
+            tv5.setText(exitroom[1]);
+
+            tv6 = (TextView) findViewById(R.id.heyamei2);
+            tv6.setText(exitroom[2]);
+
+            tv7 = (TextView) findViewById(R.id.heyaban2);
+            tv7.setText(exitroom[3]);
+
+            tv8 = (TextView) findViewById(R.id.nichiji2);
+            tv8.setText(exitroom[4]);
         }
     };
 
@@ -85,5 +113,17 @@ public class MainActivity extends Activity {
 
         tv4 = (TextView) findViewById(R.id.nichiji);
         tv4.setText("null");
+
+        tv5 = (TextView) findViewById(R.id.toumei2);
+        tv5.setText("null");
+
+        tv6 = (TextView) findViewById(R.id.heyamei2);
+        tv6.setText("null");
+
+        tv7 = (TextView) findViewById(R.id.heyaban2);
+        tv7.setText("null");
+
+        tv8 = (TextView) findViewById(R.id.nichiji2);
+        tv8.setText("null");
     }
 }
